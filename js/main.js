05b867d6fedb7e76cb636f2616b0d5afa3bfd80a
Alfeos McQueen <121122572+AlfeosMCQ@users.noreply.github.com>
2022-12-21 10:42:03 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,38 +23,10 @@ $('.slider_btn_prev').click(function() {
     owl.trigger('prev.owl.carousel', [300]);
 })
 
-$('.slider_btn_p').click(function() {
-    owl.trigger('prev.owl.carousel', [300]);
-})
 
 $('.slider_btn_next').click(function() {
     owl.trigger('next.owl.carousel');
 })
-
-$('.slider_btn_n').click(function() {
-    owl.trigger('next.owl.carousel');
-})
-
-
-$(".small").owlCarousel({
-    loop:true, //Зацикливаем слайдер
-    margin:10, //Отступ от картино если выводите больше 1
-    nav:false, //Отключил навигацию
-    autoplay:true, //Автозапуск слайдера
-    smartSpeed:2000, //Время движения слайда
-    autoplayTimeout:4000, //Время смены слайда
-    responsive:{ //Адаптация в зависимости от разрешения экрана
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:5
-        }
-    }
-});
 
 
 //nav icon
